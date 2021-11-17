da807bc921d25587f9a42124cf1fb854a40a592f
Arsh Sisodiya <arshsisodiya@gmail.com>
2021-11-17 16:31:46 +0530

    changed bot token

--- forcesubConfig.py
+++ forcesubConfig.py
@@ -2,7 +2,7 @@ import os
 
 class Config():
   #Get it from @botfather
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "1974106970:AAETAmxVApqOQG5RlR8sTaPwrNLjYOr5LRg")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "1942974533:AAEFAKdDmdHQPH8mxqNY0wZx1FyF2UaUu00")
   # Your bot updates channel username without @ or leave empty
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "heliosmirror")
   DEVELOPER = os.environ.get("DEVELOPER", "NmberSEVEN")
