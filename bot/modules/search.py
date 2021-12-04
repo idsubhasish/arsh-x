0db518bfaef6165b729f972ac0702dd4ab89127d
arshsisodiya <arshsisodiya@gmail.com>
2021-12-04 20:02:09 +0530

    removed torrent search
    search command will now search files in drive

--- bot/modules/search.py
+++ bot/modules/search.py
@@ -1,123 +1,67 @@
-import math
-import qbittorrentapi as qba
 from telegram import InlineKeyboardMarkup
-from telegram.ext import CallbackQueryHandler, CommandHandler
+from telegram.ext import CommandHandler, CallbackQueryHandler
 
-from bot import dispatcher, LOGGER, get_client, search_dict_lock, search_dict
-from bot.helper.telegram_helper.message_utils import editMessage, sendMessage
-from bot.helper.ext_utils.bot_utils import new_thread, get_readable_file_size
+from bot.helper.mirror_utils.upload_utils.gdriveTools import GoogleDriveHelper
+from bot import LOGGER, dispatcher
+from bot.helper.telegram_helper.message_utils import sendMessage, editMessage, sendMarkup
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.telegram_helper import button_build
 
-
-@new_thread
-def search(update, context):
+def list_buttons(update, context):
+    user_id = update.message.from_user.id
     try:
         key = update.message.text.split(" ", maxsplit=1)[1]
-        client = get_client()
-        search = client.search_start(pattern=str(key), plugins='all', category='all')
-        srchmsg = sendMessage("Searching...", context.bot, update)
-        user_id = update.message.from_user.id
-        search_id = search.id
-        LOGGER.info(f"qBittorrent Search: {key}")
-        while True:
-            result_status = client.search_status(search_id=search_id)
-            status = result_status[0].status
-            if status != 'Running':
-                break
-        dict_search_results = client.search_results(search_id=search_id)
-        search_results = dict_search_results.results
-        total_results = dict_search_results.total
-        if total_results != 0:
-            total_pages = math.ceil(total_results/3)
-            msg = getResult(search_results)
-            buttons = button_build.ButtonMaker()
-            if total_results > 3:
-                msg += f"<b>Pages: </b>1/{total_pages} | <b>Results: </b>{total_results}"
-                buttons.sbutton("Previous", f"srchprev {user_id} {search_id}")
-                buttons.sbutton("Next", f"srchnext {user_id} {search_id}")
-            buttons.sbutton("Close", f"closesrch {user_id} {search_id}")
-            button = InlineKeyboardMarkup(buttons.build_menu(2))
-            editMessage(msg, srchmsg, button)
-            with search_dict_lock:
-                search_dict[search_id] = client, search_results, total_results, total_pages, 1, 0
-        else:
-            editMessage(f"No result found for <i>{key}</i>", srchmsg)
     except IndexError:
-        sendMessage("Send a search key along with command", context.bot, update)
-    except Exception as e:
-        LOGGER.error(str(e))
+        return sendMessage('Send a search key along with command', context.bot, update)
+    buttons = button_build.ButtonMaker()
+    buttons.sbutton("Drive Root", f"types {user_id} root")
+    buttons.sbutton("Recursive", f"types {user_id} recu")
+    buttons.sbutton("Cancel", f"types {user_id} cancel")
+    button = InlineKeyboardMarkup(buttons.build_menu(2))
+    sendMarkup('Choose option to list.', context.bot, update, button)
 
-def searchPages(update, context):
+def select_type(update, context):
     query = update.callback_query
     user_id = query.from_user.id
+    msg = query.message
+    key = msg.reply_to_message.text.split(" ", maxsplit=1)[1]
     data = query.data
     data = data.split(" ")
-    search_id = int(data[2])
     if user_id != int(data[1]):
         query.answer(text="Not Yours!", show_alert=True)
-        return
-    with search_dict_lock:
-        try:
-            client, search_results, total_results, total_pages, pageNo, start = search_dict[search_id]
-        except:
-            query.answer(text="Old Result", show_alert=True)
-            query.message.delete()
-            return
-    if data[0] == "srchnext":
-        if pageNo == total_pages:
-            start = 0
-            pageNo = 1
-        else:
-            start += 3
-            pageNo += 1
-    elif data[0] == "srchprev":
-        if pageNo == 1:
-            pageNo = total_pages
-            start = 3 * (total_pages -1)
-        else:
-            pageNo -= 1
-            start -= 3
-    elif data[0] == "closesrch":
-        client.search_delete(search_id)
-        client.auth_log_out()
-        with search_dict_lock:
-            try:
-                del search_dict[search_id]
-            except:
-                pass
-        query.message.delete()
-        return
-    with search_dict_lock:
-        search_dict[search_id] = client, search_results, total_results, total_pages, pageNo, start
-    msg = getResult(search_results, start=start)
-    msg += f"<b>Pages: </b>{pageNo}/{total_pages} | <b>Results: </b>{total_results}"
-    buttons = button_build.ButtonMaker()
-    buttons.sbutton("Previous", f"srchprev {user_id} {search_id}")
-    buttons.sbutton("Next", f"srchnext {user_id} {search_id}")
-    buttons.sbutton("Close", f"closesrch {user_id} {search_id}")
-    button = InlineKeyboardMarkup(buttons.build_menu(2))
-    query.answer()
-    editMessage(msg, query.message, button)
+    elif data[2] in ["root", "recu"]:
+        query.answer()
+        buttons = button_build.ButtonMaker()
+        buttons.sbutton("Folders", f"types {user_id} folders {data[2]}")
+        buttons.sbutton("Files", f"types {user_id} files {data[2]}")
+        buttons.sbutton("Both", f"types {user_id} both {data[2]}")
+        buttons.sbutton("Cancel", f"types {user_id} cancel")
+        button = InlineKeyboardMarkup(buttons.build_menu(2))
+        editMessage('Choose option to list.', msg, button)
+    elif data[2] in ["files", "folders", "both"]:
+        query.answer()
+        list_method = data[3]
+        item_type = data[2]
+        editMessage(f"<b>Searching for <i>{key}</i></b>", msg)
+        list_drive(key, msg, list_method, item_type)
+    else:
+        query.answer()
+        editMessage("list has been canceled!", msg)
 
-def getResult(search_results, start=0):
-    msg = ""
-    for index, result in enumerate(search_results[start:], start=1):
-        msg += f"<a href='{result.descrLink}'>{result.fileName}</a>\n"
-        msg += f"<b>Size: </b>{get_readable_file_size(result.fileSize)}\n"
-        msg += f"<b>Seeders: </b>{result.nbSeeders} | <b>Leechers: </b>{result.nbLeechers}\n"
-        msg += f"<b>Link: </b><code>{result.fileUrl}</code>\n\n"
-        if index == 3:
-            break
-    return msg
+
+def list_drive(key, bmsg, list_method, item_type):
+    LOGGER.info(f"listing: {key}")
+    list_method = list_method == "recu"
+    gdrive = GoogleDriveHelper()
+    msg, button = gdrive.drive_list(key, isRecursive=list_method, itemType=item_type)
+    if button:
+        editMessage(msg, bmsg, button)
+    else:
+        editMessage(f'No result found for <i>{key}</i>', bmsg)
 
 
 search_handler = CommandHandler(BotCommands.SearchCommand, search, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
-srchnext_handler = CallbackQueryHandler(searchPages, pattern="srchnext", run_async=True)
-srchprevious_handler = CallbackQueryHandler(searchPages, pattern="srchprev", run_async=True)
-delsrch_handler = CallbackQueryHandler(searchPages, pattern="closesrch", run_async=True)
-dispatcher.add_handler(search_handler)
-dispatcher.add_handler(srchnext_handler)
-dispatcher.add_handler(srchprevious_handler)
-dispatcher.add_handler(delsrch_handler)
+list_type_handler = CallbackQueryHandler(select_type, pattern="types", run_async=True)
+dispatcher.add_handler(list_handler)
+dispatcher.add_handler(list_type_handler)
