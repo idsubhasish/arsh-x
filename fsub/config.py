e733c531b500128bc4bfd9c5ebba22e761e64a50
arshsisodiya <arshsisodiya@gmail.com>
2021-12-15 22:31:37 +0530

    added fsub module
    user have to fill configs for Fsub manually

---
+++ fsub/config.py
@@ -0,0 +1,11 @@
+import os
+
+
+class Config(object):
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+
+    APP_ID = int(os.environ.get("APP_ID",))
+
+    API_HASH = os.environ.get("API_HASH", "")
+
+    CHANNEL_USERNAME = os.environ.get("CHANNEL_USERNAME", "@heliosmirror")
