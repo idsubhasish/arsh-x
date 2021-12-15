3b5d20bf9a81707bd1a83e4ad37b6349017b5365
arshsisodiya <arshsisodiya@gmail.com>
2021-12-15 19:49:20 +0530

    defined usage command

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -50,5 +50,6 @@ class _BotCommands:
         self.QbZipLeechCommand = getCommand('QBZIPLEECH_COMMAND', 'qbzipleech')
         self.LeechWatchCommand = getCommand('LEECHWATCH_COMMAND', 'leechwatch')
         self.LeechZipWatchCommand = getCommand('LEECHZIPWATCH_COMMAND', 'leechzipwatch')
-        self.TorrentSearchCommand = getCommand('TOR_COMMAND', 'TS')
+        self.TorrentSearchCommand = getCommand('TOR_COMMAND', 'ts')
+        self.UsageCommand = getCommand('USAGE_COMMAND', 'usage')
 BotCommands = _BotCommands()
