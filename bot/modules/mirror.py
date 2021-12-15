439c873b920107a558ed9777599a4546ad25968a
arshsisodiya <arshsisodiya@gmail.com>
2021-12-15 20:02:00 +0530

    tyding up

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -221,7 +221,7 @@ class MirrorListener(listeners.MirrorListeners):
                 sendMessage(msg, self.bot, self.update)
             else:
                 chat_id = str(self.message.chat.id)[4:]
-                msg += f'\n<b>cc: </b>{uname}\n\n'
+                msg += f'\n<b>Leeched By: </b>{uname}\n\n'
                 fmsg = ''
                 for index, item in enumerate(list(files), start=1):
                     msg_id = files[item]
