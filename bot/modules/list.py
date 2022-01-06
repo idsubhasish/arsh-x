4f4ebd858e29b283c99f6e9edf940d8a66608254
arshsisodiya <arshsisodiya@gmail.com>
2022-01-06 22:30:58 +0530

    changing list button names

--- bot/modules/list.py
+++ bot/modules/list.py
@@ -17,8 +17,8 @@ def list_buttons(update, context):
     except IndexError:
         return sendMessage('Send a search key along with command', context.bot, update)
     buttons = button_build.ButtonMaker()
-    buttons.sbutton("Drive Root", f"types {user_id} root")
-    buttons.sbutton("Recursive", f"types {user_id} recu")
+    buttons.sbutton("Normal", f"types {user_id} root")
+    buttons.sbutton("Deep", f"types {user_id} recu")
     buttons.sbutton("Cancel", f"types {user_id} cancel")
     button = InlineKeyboardMarkup(buttons.build_menu(2))
     sendMarkup('Choose option to list.', context.bot, update, button)
